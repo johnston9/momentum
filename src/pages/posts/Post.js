e2fc6e4214f6c93d969ca254a41802182e6aaadc
johnston9 <davejohnston9@gmail.com>
2021-10-30 19:52:28 +0000

    posts

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Avatar from "../../components/Avatar";
+import { axiosRes } from '../../api/axiosDefaults';
 
 const Post = (props) => {
     const {
@@ -19,14 +20,48 @@ const Post = (props) => {
         image,
         updated_at,
         postPage,
+        setPosts,
       } = props;
 
     const currentUser = useCurrentUser()
     const is_owner = currentUser?.username === owner;
+    
+    const handleLike = async () => {
+        try {
+          const { data } = await axiosRes.post("/likes/", { post: id });
+          setPosts((prevPosts) => ({
+            ...prevPosts,
+            results: prevPosts.results.map((post) => {
+              return post.id === id
+                ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+                : post;
+            }),
+          }));
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      const handleUnlike = async () => {
+        try {
+          await axiosRes.delete(`/likes/${like_id}/`);
+          setPosts((prevPosts) => ({
+            ...prevPosts,
+            results: prevPosts.results.map((post) => {
+              return post.id === id
+                ? { ...post, likes_count: post.likes_count - 1, like_id: null }
+                : post;
+            }),
+          }));
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
     return (
         <Card className={styles.Post}>
             <Card.Body>
-                <div className="align-items-center justify-content-between">
+                <div className="d-flex align-items-center justify-content-between">
                     <Link to={`/profiles/${profile_id}`}>
                         <Avatar src={profile_image} height={55}  />
                         {owner}
@@ -50,11 +85,11 @@ const Post = (props) => {
                               <i className="far fa-heart" />
                         </OverlayTrigger>
                     ) : like_id ? (
-                        <span onClick={() => {}}>
+                        <span onClick={handleUnlike}>
                             <i className={`fas fa-heart ${styles.Heart}`} />
                         </span>
                     ) : currentUser ? (
-                        <span onClick={() => {}}>
+                        <span onClick={handleLike}>
                             <i className={`far fa-heart ${styles.HeartOutline}`} />
                         </span>
                     ) : (
