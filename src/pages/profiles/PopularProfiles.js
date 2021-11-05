7ce66ecca600ac53450893d25cbe29efcd4a497d
johnston9 <davejohnston9@gmail.com>
2021-11-05 21:26:51 +0000

    profiles2

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,12 +1,39 @@
-import React from 'react';
+// import { useProfileData } from '../../contexts/ProfileDataContext';
+// import Profile from "./Profile";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import appStyles from "../../App.module.css";
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from "../../components/Asset";
-import { useProfileData } from '../../contexts/ProfileDataContext';
-// import Profile from "./Profile";
+
 
 const PopularProfiles = () => {
-    const { popularProfiles } = useProfileData;
+    const [profileData, setProfileData] = useState({
+        // we will use the pageProfile later!
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+        });
+
+    const { popularProfiles } = profileData;
+    const currentUser = useCurrentUser;
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const {data} = await axios.get(
+                    '/profiles/?ordering=-followers_count'
+                );
+                setProfileData(prevState => ({
+                    ...prevState,
+                    popularProfiles: data,
+                }))
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        handleMount()
+    }, [currentUser])
 
     return (
         <Container className={appStyles.Content}>
