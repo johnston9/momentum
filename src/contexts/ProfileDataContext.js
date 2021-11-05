718301e5f320ba8ad39bad2dde34e8e0c32a9d19
johnston9 <davejohnston9@gmail.com>
2021-11-05 21:20:05 +0000

    profiles

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,45 @@
+import { useCurrentUser } from "./CurrentUserContext";
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+    const [profileData, setProfileData] = useState({
+        // we will use the pageProfile later
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+      });
+     
+      const currentUser = useCurrentUser();
+     
+      useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const { data } = await axiosReq.get(
+              "/profiles?ordering=-followers_count"
+            );
+            console.log(data)
+            setProfileData((prevState) => ({
+              ...prevState,
+              results: data,
+            }));
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        handleMount();
+      }, [currentUser]);
+
+      return (
+        <ProfileDataContext.Provider value={profileData}>
+            <SetProfileDataContext.Provider value={setProfileData}>
+                {children}
+            </SetProfileDataContext.Provider>
+        </ProfileDataContext.Provider>
+      )
+}
--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,36 +1,12 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import appStyles from "../../App.module.css";
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from "../../components/Asset";
+import { useProfileData } from '../../contexts/ProfileDataContext';
+// import Profile from "./Profile";
 
 const PopularProfiles = () => {
-    const [profileData, setProfileData] = useState({
-        // we will use the pageProfile later!
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-        });
-
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser;
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const {data} = await axios.get(
-                    '/profiles/?ordering=-followers_count'
-                );
-                setProfileData(prevState => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }))
-            } catch(err) {
-                console.log(err)
-            }
-        }
-        handleMount()
-    }, [currentUser])
+    const { popularProfiles } = useProfileData;
 
     return (
         <Container className={appStyles.Content}>
@@ -44,6 +20,7 @@ const PopularProfiles = () => {
             ) : (
                 <Asset spinner />
             )}
+
         </Container>
     )
 }
